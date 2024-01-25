5ed29924563265c5f69c2a9edad9000f3e825df1
MJW0651 <michael.woolcott96@gmail.com>
2024-01-24 20:28:17 -0600

    Initial commit.

--- sheetCombiner.py
+++ sheetCombiner.py
@@ -1,4 +1,4 @@
-# Gather and combine .xls, .xlsx, .csv, etc. files and combine them into a single worksheet/workbook
+# Gather and combine .xlsx and .csv files and combine them into a single worksheet/workbook
 # Michael Woolcott
 
 import openpyxl
